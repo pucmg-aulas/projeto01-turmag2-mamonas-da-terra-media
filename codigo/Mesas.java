e6ef879e7dcb86a9dff3d1044ea499f829295b64
Ian Matsuhara <86268101+YoshuJumpMaster@users.noreply.github.com>
2024-04-15 20:01:20 -0300

    Update Mesas.java

--- codigo/Mesas.java
+++ codigo/Mesas.java
@@ -1,39 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package restaurante;
-
 import java.util.*;
 
 public class Mesa {
-  
+
   private int numeroAssentos;
   private boolean estaDisponivel;
 
-  public Mesa(int numeroAssentos, boolean estaDisponivel){
+  public Mesa(int numeroAssentos, true) { // adiciona uma mesa recebendo um parametro de assentos por fora.
+    if (numeroAssentos < 1) {
+      throw new IllegalArgumentException("A mesa deve ter no mínimo um assento, se não, não é uma mesa");
+    }
     this.numeroAssentos = numeroAssentos;
     this.estaDisponivel = estaDisponivel;
   }
 
-  public int getNumeroAssentos(){
+  public int getNumeroAssentos() {
     return this.numeroAssentos;
   }
-  public boolean getEstaDisponivel(){
+
+  public boolean isDisponivel() {
     return this.estaDisponivel;
   }
-  
 
   public void ocuparMesa() {
-    if (this.estaDisponivel == false) {
-      this.estaDisponivel = true;
-    }
+    this.estaDisponivel = false;
   }
 
   public void desocuparMesa() {
-    if (this.estaDisponivel == true) {
-      this.estaDisponivel = false;
-    }
+    this.estaDisponivel = true;
   }
 
 }
