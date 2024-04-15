3f1f7c66a22a619df8eac64ed44fa453f3c90d7a
Ian Matsuhara <86268101+YoshuJumpMaster@users.noreply.github.com>
2024-04-15 20:13:55 -0300

    Update Main.java

--- codigo/Main.java
+++ codigo/Main.java
@@ -8,25 +8,10 @@ public class Main {
     ArrayList<RequisicaoDeMesa> a = new ArrayList<>();
     ListaDeEspera le = new ListaDeEspera(a);
 
-    LocalTime lo1;// = LocalTime.of(14,30);
-    LocalTime lo2;// = LocalTime.of(17,30);
-    RequisicaoDeMesa jorge = new RequisicaoDeMesa("Jorge", 8, lo1 = LocalTime.of(14, 30), lo2 = LocalTime.of(17, 30));
-    lo1 = LocalTime.of(17, 0);
-    lo2 = LocalTime.of(20, 10);
-    RequisicaoDeMesa tadeu = new RequisicaoDeMesa("Tadeu", 4, lo1, lo2);
-    lo1 = LocalTime.of(06, 0);
-    lo2 = LocalTime.of(07, 56);
-    RequisicaoDeMesa mateus = new RequisicaoDeMesa("Mateus", 6, lo1, lo2);
-    lo1 = LocalTime.of(20, 15);
-    lo2 = LocalTime.of(22, 30);
-    RequisicaoDeMesa joinha = new RequisicaoDeMesa("Joinha", 6, lo1, lo2);
-    le.adicionaNaLista(jorge);
-    le.adicionaNaLista(tadeu);
-    le.adicionaNaLista(mateus);
-    le.adicionaNaLista(joinha);
-    System.out.println(le.imprimeLista());
-    le.removeDaLista(mateus);
-    System.out.println(le.imprimeLista());
+    le.adicionaNaLista(new RequisicaoDeMesa("Jorge", 8, LocalTime.of(14, 30), LocalTime.of(17, 30)));
+    le.adicionaNaLista(new RequisicaoDeMesa("Tadeu", 4, LocalTime.of(17, 0), LocalTime.of(20, 10)));
+    le.adicionaNaLista(new RequisicaoDeMesa("Mateus", 6, LocalTime.of(6, 0), LocalTime.of(7, 56)));
+    le.adicionaNaLista(new RequisicaoDeMesa("Joinha", 6, LocalTime.of(20, 15), LocalTime.of(22, 30)));
   
 
     Mesa m1 = new Mesa(4, false);
