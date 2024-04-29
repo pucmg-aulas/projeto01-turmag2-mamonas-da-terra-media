df94dd50cfc90b5cfb08555cb5dcf93febd8f23d
The new blood <65373363+MoraisGordo@users.noreply.github.com>
2024-04-29 07:32:18 -0300

    Update Main.java
    Colocar ultimos metodos

--- restaurante/Main.java
+++ restaurante/Main.java
@@ -7,6 +7,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ListaDeEspera listaDeEspera = new ListaDeEspera();
+        Mesas m1 = new Mesas(4,true);
+        Mesas m2 = new Mesas(6,true);
+        Mesas m3 = new Mesas(8,true);
 
         while (true) {
             System.out.println("\nEscolha uma opcao:");
@@ -14,7 +17,10 @@ public class Main {
             System.out.println("2 - Remover pessoa da fila de espera");
             System.out.println("3 - Buscar pessoa na fila de espera");
             System.out.println("4 - Mostrar toda a lista de espera");
-            System.out.println("5 - Sair");
+            System.out.println("5 - Mostrar disponibilidade das mesas");
+            System.out.println("6 - Desocupar Mesa");
+            System.out.println("7 - Ocupar Mesa");
+            System.out.println("8 - Sair");
 
             System.out.print("Opcao: ");
             int opcao = scanner.nextInt();
@@ -51,10 +57,52 @@ public class Main {
 
             } else if (opcao == 5){
                 
+                 System.out.println("\nMesas:");
+                System.out.println("Mesa 4 lugares: " + m1.isDisponivel());
+                System.out.println("Mesa 6 lugares: " + m2.isDisponivel());
+                System.out.println("Mesa 8 lugares: " + m3.isDisponivel());
+                
+            } else if (opcao == 6) {
+              
+               System.out.println("\nDesocupar mesa :");
+               System.out.println("1- Mesa 4 lugares ");
+                System.out.println("2- Mesa 6 lugares ");
+                System.out.println("3- Mesa 8 lugares ");
+               int x = scanner.nextInt();
+               if (x == 1){
+     
+                   m1.desocuparMesa();
+               }
+               if (x == 2){
+                   m2.desocuparMesa();
+               }
+               if (x == 3){
+                   m3.desocuparMesa();
+               }
+                
+            }
+            else if (opcao == 7) {
+               
+                System.out.println("\nOcupar mesa :");
+               System.out.println("1- Mesa 4 lugares ");
+               System.out.println("2- Mesa 6 lugares ");
+               System.out.println("3- Mesa 8 lugares ");
+               int x = scanner.nextInt();
+               if (x == 1){
+     
+                   m1.ocuparMesa();
+               }
+               if (x == 2){
+                   m2.ocuparMesa();
+               }
+               if (x == 3){
+                   m3.ocuparMesa();
+               }
+                
+            }else if (opcao == 8) {
                 System.out.println("\nEncerrando programa.");
                 break;
-                
-            } else {
+            }else {
                 
                 System.out.println("\nOpção inválida. Tente novamente.");
             }
@@ -62,3 +110,4 @@ public class Main {
         scanner.close();
     }
 }
+
