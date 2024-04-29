a67b99d61a431d8635c44288a76077ae4e0e40e1
The new blood <65373363+MoraisGordo@users.noreply.github.com>
2024-04-28 22:12:09 -0300

    Update Main.java
    Adicionar opçao de ver a lista de espera

--- restaurante/Main.java
+++ restaurante/Main.java
@@ -9,18 +9,20 @@ public class Main {
         ListaDeEspera listaDeEspera = new ListaDeEspera();
 
         while (true) {
-            System.out.println("\nEscolha uma opção:");
+            System.out.println("\nEscolha uma opcao:");
             System.out.println("1 - Incluir pessoa na fila de espera");
             System.out.println("2 - Remover pessoa da fila de espera");
             System.out.println("3 - Buscar pessoa na fila de espera");
-            System.out.println("4 - Sair");
-            System.out.print("Opção: ");
+            System.out.println("4 - Mostrar toda a lista de espera");
+            System.out.println("5 - Sair");
+
+            System.out.print("Opcao: ");
             int opcao = scanner.nextInt();
             scanner.nextLine(); 
 
             if (opcao == 1) {
 
-                System.out.print("Digite o nome do cliente: ");
+                System.out.print("\nDigite o nome do cliente: ");
                 String nome = scanner.nextLine();
                 System.out.print("Digite o número de assentos: ");
                 int lugares = scanner.nextInt();
@@ -31,25 +33,30 @@ public class Main {
 
             } else if (opcao == 2) {
 
-                System.out.print("Digite o nome do cliente: ");
+                System.out.print("\nDigite o nome do cliente: ");
                 String nome = scanner.nextLine();
                 listaDeEspera.removerDaListaPorNome(nome);
                 System.out.println("Cliente removido com sucesso!");
 
             } else if (opcao == 3) {
 
-                System.out.print("Digite o nome do cliente: ");
+                System.out.print("\nDigite o nome do cliente: ");
                 String nome = scanner.nextLine();
                 System.out.println(listaDeEspera.imprimirCliente(nome));
 
             } else if (opcao == 4) {
 
-                System.out.println("Encerrando o programa.");
-                break;
+                System.out.println("\nLista de espera: ");
+                System.out.println(listaDeEspera.imprimirLista());
 
+            } else if (opcao == 5){
+                
+                System.out.println("\nEncerrando programa.");
+                break;
+                
             } else {
                 
-                System.out.println("Opção inválida. Tente novamente.");
+                System.out.println("\nOpção inválida. Tente novamente.");
             }
         }
         scanner.close();
