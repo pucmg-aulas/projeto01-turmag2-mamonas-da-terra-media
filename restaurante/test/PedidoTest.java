f2d6f40677904b47fc4489054b94397786d33dbd
Davi Augusto <113954562+daviaugustoo@users.noreply.github.com>
2024-05-13 07:42:57 -0300

    Update PedidoTest.java

--- restaurante/test/PedidoTest.java
+++ restaurante/test/PedidoTest.java
@@ -21,45 +21,4 @@ class PedidoTest {
         assertEquals(pedidoEsperado, pedido.imprimePedido(), "Não deveria haver nenhum pedido registrado");
     }
 
-    @Test
-    void registraPedidoMoquecaSucoTest() {
-        Pedido pedido = new Pedido();
-        pedido.fazerPedido(new int[]{1, 8, 0});
-        String pedidoEsperado = "Moqueca De Tilapia, Suco, ";
-        assertEquals(pedidoEsperado, pedido.imprimePedido(), "Pedido não foi registrado corretamente");
-    }
-
-    @Test
-    void registraPedidoFalafelCervejaTest() {
-        Pedido pedido = new Pedido();
-        pedido.fazerPedido(new int[]{2, 10, 0});
-        String pedidoEsperado = "Falafel Assado, Cerveja, ";
-        assertEquals(pedidoEsperado, pedido.imprimePedido(), "Pedido não foi registrado corretamente");
-    }
-
-
-    @Test
-    void registraPedidoSaladaAguaTest() {
-        Pedido pedido = new Pedido();
-        pedido.fazerPedido(new int[]{3, 7, 0});
-        String pedidoEsperado = "Salada Primavera com Macarrão Konjac, Agua, ";
-        assertEquals(pedidoEsperado, pedido.imprimePedido(), "Pedido não foi registrado corretamente");
-    }
-
-    @Test
-    void registraPedidoEscondidinhoVinhoTest() {
-        Pedido pedido = new Pedido();
-        pedido.fazerPedido(new int[]{4, 11, 0});
-        String pedidoEsperado = "Escondidinho de Frango, Taca de Vinho, ";
-        assertEquals(pedidoEsperado, pedido.imprimePedido(), "Pedido não foi registrado corretamente");
-    }
-
-    @Test
-    void registraPedidoStrogonoffRefrigeranteTest() {
-        Pedido pedido = new Pedido();
-        pedido.fazerPedido(new int[]{5, 9, 0});
-        String pedidoEsperado = "Strogonoff, Refrigerante, ";
-        assertEquals(pedidoEsperado, pedido.imprimePedido(), "Pedido não foi registrado corretamente");
-    }
-
 }
