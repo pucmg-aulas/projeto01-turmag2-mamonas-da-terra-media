a165c40c1c263e411926d4e8bd203aa922696489
The new blood <65373363+MoraisGordo@users.noreply.github.com>
2024-05-19 11:53:18 -0300

    Update Pedido.java
    Versao final pedido

--- restaurante/Pedido.java
+++ restaurante/Pedido.java
@@ -1,76 +1,99 @@
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package restaurante;
 import java.util.*;
 
-public class Pedido extends Menu {
+public class Pedido{
     
-    double valorPedido = 0;
-    String pedido = "";
+    ArrayList itens = new ArrayList();
+    ArrayList<Double> valores = new ArrayList<>();
     
     public Pedido(){}
      
     public void fazerPedido(){
         
+        Menu cardapio = new Menu();
         Scanner ler = new Scanner(System.in);
-        System.out.println(imprimeMenuComida());
-        System.out.println(imprimeMenuBebida());
+        System.out.println(cardapio.imprimirMenu());
         System.out.println("Aperte 0 para encerrar!");
         System.out.println("Numeros do Pedido: ");
         int x = ler.nextInt();
         while(x != 0){
             if(x == 1){
-                this.valorPedido += MOQUECA_DE_TILAPIA;
-                this.pedido += "Moqueca De Tilapia, ";              
+                itens.add("Moqueca De Tilapia");
+                valores.add(cardapio.getMoqueca());
+                x = ler.nextInt();
             }
             if(x == 2){
-                this.valorPedido += FALAFEL_ASSADO;
-                this.pedido += "Falafel Assado, ";              
+                itens.add("Falafel Assado");
+                valores.add(cardapio.getFalafel());
+                x = ler.nextInt();
+                
             }
             if(x == 3){
-                this.valorPedido += SALADA_PRIMAVERA_COM_MACARRAO;
-                this.pedido += "Salada Primavera com Macarrão Konjac, ";              
+                itens.add("Salada Primavera com Macarrao Konjac");
+                valores.add(cardapio.getSalada());
+                x = ler.nextInt();
             }
             if(x == 4){
-                this.valorPedido += ESCONDIDINHO_DE_FRANGO;
-                this.pedido += "Escondidinho de Frango, ";              
+                itens.add("Escondidinho de Frango");
+                valores.add(cardapio.getEscondidinho());
+                x = ler.nextInt();
             }
             if(x == 5){
-                this.valorPedido += STROGONOFF;
-                this.pedido += "Strogonoff, ";              
+                itens.add("Strogonoff");
+                valores.add(cardapio.getGostrognocoff());
+                x = ler.nextInt();
             }
             if(x == 6){
-                this.valorPedido += CACAROLA_DE_CARNE_COM_LEGUMES;
-                this.pedido += "Cacarola de Carne com Legumes, ";              
+                itens.add("Cacarola de Carne com Legumes");
+                valores.add(cardapio.getCacarola());
+                x = ler.nextInt();
             }
             if(x == 7){
-                this.valorPedido += AGUA;
-                this.pedido += "Agua, ";              
+                itens.add("Agua");
+                valores.add(cardapio.getAgua());
+                x = ler.nextInt();
+                
             }
             if(x == 8){
-                this.valorPedido += SUCO;
-                this.pedido += "Suco, ";              
+                itens.add("Suco");
+                valores.add(cardapio.getSuco());
+                x = ler.nextInt();
             }
             if(x == 9){
-                this.valorPedido += REFRIGERANTE;
-                this.pedido += "Refrigerante, ";              
+                itens.add("Refrigerante");
+                valores.add(cardapio.getRefrigerante());
+                x = ler.nextInt();
             }
             if(x == 10){
-                this.valorPedido += CERVEJA;
-                this.pedido += "Cerveja, ";              
+                itens.add("Cerveja");
+                valores.add(cardapio.getCerveja());
+                x = ler.nextInt();
             }
             if(x == 11){
-                this.valorPedido += VINHO;
-                this.pedido += "Taca de Vinho, ";              
-            }
-            x = ler.nextInt();
+                itens.add("Taca De Vinho");
+                valores.add(cardapio.getTacaDeVinho());
+                x = ler.nextInt();
+            }       
         }
     }
     
-    public String imprimePedido(){
-        return this.pedido;
+    public ArrayList getPedido(){
+        return this.itens;
+    }
+    
+    public double getValores(int i){
+        return this.valores.get(i);
+    }
+    public String imprimirPedido(){
+        String itens = "";
+        for(int i = 0; i<this.itens.size(); i++){
+            itens += this.itens.get(i) + ", ";
+        }
+        return itens;
     }
 }
+
+
+
+     
