fd810879db8b6ede775db444a2f102bae5b6e074
The new blood <65373363+MoraisGordo@users.noreply.github.com>
2024-05-19 11:50:15 -0300

    Update Menu.java
    Mudança de como funciona o menu

--- restaurante/Menu.java
+++ restaurante/Menu.java
@@ -1,36 +1,87 @@
 package restaurante;
+import java.util.*;
 
 
-public abstract class Menu {
-    
+public class Menu {        
         
-        final double MOQUECA_DE_TILAPIA = 600;
-        final double FALAFEL_ASSADO = 500;
-        final double SALADA_PRIMAVERA_COM_MACARRAO = 1000;
-        final double ESCONDIDINHO_DE_FRANGO = 400;
-        final double STROGONOFF = 300;
-        final double CACAROLA_DE_CARNE_COM_LEGUMES = 1300;
-        final double AGUA = 20;
-        final double SUCO = 50;
-        final double REFRIGERANTE = 40;
-        final double CERVEJA = 100;
-        final double VINHO = 300;
+        ArrayList itens = new ArrayList();
+        ArrayList<Double> preco = new ArrayList();
         
-    
-    public String imprimeMenuComida(){
+        public Menu(){
+             itens.add("Moqueca De Tilapia");
+             preco.add(1000.00);             
+             itens.add("Falafel Assado");
+             preco.add(1000.00);
+             itens.add("Salada Primavera com Macarrão Konjac");
+             preco.add(1000.00);
+             itens.add("Escondidinho de frango");
+             preco.add(1000.00);
+             itens.add("Strogonoff");
+             preco.add(1000.00);
+             itens.add("Cacarola de Carne com Legumes");
+             preco.add(1000.00);
+             itens.add("Agua");
+             preco.add(1000.00);
+             itens.add("Suco");
+             preco.add(1000.00);
+             itens.add("Refrigerante");
+             preco.add(1000.00);
+             itens.add("Cerveja");
+             preco.add(1000.00);
+             itens.add("Taca de Vinho");
+             preco.add(1000.00);
+             String itens = "";
+             for(int i=0; i<this.itens.size();i++){
+             itens += this.itens.get(i) + ", ";
+        }
+             System.out.println(itens);
+        }
+
+        
+        
+    public static String imprimirMenu(){
         String menu = "";
-        menu += "******Comidas******\n *1 - Moqueca de Tilapia *\n*2- Falafel Assado *\n*3 - Salada Primavera com Macarrão Konjac*\n";
-        menu += "*4 - Escondidinho de Frango *\n*5 - Strogonoff *\n*6 - Caçarola de Carne com Legumes*";
+        menu += "******************Comidas******************\n*1 - Moqueca de Tilapia                   *\n*2 - Falafel Assado                       *\n*3 - Salada Primavera com Macarrao Konjac *\n";
+        menu += "*4 - Escondidinho de Frango               *\n*5 - Gostrognocoff                        *\n*6 - Cacarola de Carne com Legumes        *\n*******************************************";
+        menu += "\n*******Bebidas*******\n*7 - Agua           *\n*8 - Suco           *\n";
+        menu += "*9 - Refrigerante   *\n*10 - Cerveja       *\n*11 - Taca de Vinho *\n*********************";
         return menu;
     }
     
-    public String imprimeMenuBebida(){
-        String menu = "";
-        menu += "******Bebidas******\n *7 - Agua *\n*8- Suco *\n";
-        menu += "*9 - Refrigerante *\n*10 - Cerveja *\n*11 - Taça de Vinho*";
-        return menu;
+    
+    public double getMoqueca(){
+        return this.preco.get(1);
     }
-    
-    
+    public double getFalafel(){
+        return this.preco.get(2);
+    }
+    public double getSalada(){
+        return this.preco.get(3);
+    }
+    public double getEscondidinho(){
+        return this.preco.get(4);
+    }
+    public double getGostrognocoff(){
+        return this.preco.get(5);
+    }
+    public double getCacarola(){
+        return this.preco.get(6);
+    }
+    public double getAgua(){
+        return this.preco.get(7);
+    }
+    public double getSuco(){
+        return this.preco.get(8);
+    }
+    public double getRefrigerante(){
+        return this.preco.get(9);
+    }
+    public double getCerveja(){
+        return this.preco.get(10);
+    }
+    public double getTacaDeVinho(){
+        return this.preco.get(11);
+    }
 }
-
+    
+   
