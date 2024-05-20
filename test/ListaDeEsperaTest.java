9ab3c9eaafe43f23e32607b13b41ac917b802dd0
Davi Augusto <113954562+daviaugustoo@users.noreply.github.com>
2024-05-20 07:47:03 -0300

    Update ListaDeEsperaTest.java

--- test/ListaDeEsperaTest.java
+++ test/ListaDeEsperaTest.java
@@ -8,25 +8,28 @@ import restaurante.RequisicaoDeMesa;
 import restaurante.Mesa;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 class ListaDeEsperaTest {
 
 	@Test
-	void adicionaVazioListaDeEsperaTest() {
-		ListaDeEspera listaTest = new ListaDeEspera();
-		assertThrows(IllegalArgumentException.class, () ->{
-			listaTest.adicionarNaLista(null);
-		});
-	}
+    void adicionaVazioListaDeEsperaTest() {
+        ArrayList<Mesa> mesasDisponiveis = new ArrayList<>();
+        mesasDisponiveis.add(new Mesa(4, true));
+        ListaDeEspera listaTest = new ListaDeEspera(mesasDisponiveis);
+        assertThrows(IllegalArgumentException.class, () -> {
+            listaTest.adicionarNaLista(null);
+        });
+    }
 
-	@Test
-	void adicionaClienteNaListaDeEsperaTest() {
-		Mesa mesa = new Mesa(4, true);
-		ListaDeEspera listaTest = new ListaDeEspera();
-		RequisicaoDeMesa requisicao = new RequisicaoDeMesa("João", 4, LocalTime.of(12, 30), mesa);
-		listaTest.adicionarNaLista(requisicao);
-		String listaEsperada = "Cliente: João, Lugares: 4, Mesa: 4 lugares\n";
-		assertEquals(listaEsperada, listaTest.imprimirLista(), "Cliente não foi adicionado corretamente à lista de espera");
-	}
+    @Test
+    void adicionaClienteNaListaDeEsperaTest() {
+        Mesa mesa = new Mesa(4, true);
+        ListaDeEspera listaTest = new ListaDeEspera(new ArrayList<>());
+        RequisicaoDeMesa requisicao = new RequisicaoDeMesa("João", 4, LocalTime.of(12, 30), mesa);
+        listaTest.adicionarNaLista(requisicao);
+        String listaEsperada = "Cliente: João, Lugares: 4, Mesa: 4 lugares\n";
+        assertEquals(listaEsperada, listaTest.imprimirLista(), "Cliente não foi adicionado corretamente à lista de espera");
+    }
 
 }
