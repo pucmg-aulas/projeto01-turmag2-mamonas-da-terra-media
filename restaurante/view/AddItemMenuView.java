2de567356d68dbf13d050f4fee63aece5177d4eb
The new blood <65373363+MoraisGordo@users.noreply.github.com>
2024-06-18 14:11:50 -0300

    Update AddItemMenuView.java

--- restaurante/view/AddItemMenuView.java
+++ restaurante/view/AddItemMenuView.java
@@ -1,1 +1,41 @@
 
+package controlers;
+
+import restaurante.ItemMenu;
+import restaurante.Menu;
+import views.AddItemMenuView;
+
+public class AddItemMenuController {
+
+    private AddItemMenuView view;
+    private Menu menu;
+
+    public AddItemMenuController() {
+        this.view = new AddItemMenuView();
+        this.menu = Menu.getInstance();
+
+        this.view.getBtnSalvar().addActionListener((e)
+                -> adicionarAoMenu());
+
+        this.view.getBtnCancelar().addActionListener((e)
+                -> cancelar());
+        
+        this.view.setVisible(true);
+    }
+
+    public void adicionarAoMenu() {
+        String nome = view.getTxtNome().getText();
+        double valor = Double.valueOf(view.getTxtValor().getText());
+        try {
+            ItemMenu item = new ItemMenu(nome, valor);
+            menu.adicionarItem(item);
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void cancelar() {
+        this.view.dispose();
+    }
+
+}
